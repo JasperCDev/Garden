f60cba7cd8c8ed9495f05bb4219b1199c5729376
JasperCDev <jasperischauvin@gmail.com>
2022-06-28 10:52:25 -0500

    add fish component

---
+++ src/components/Fish.tsx
@@ -0,0 +1,5 @@
+import styles from "./Fish.module.css";
+
+export default function Fish() {
+  return <div class={styles.fish}></div>;
+}
--- src/components/Pond.tsx
+++ src/components/Pond.tsx
@@ -1,5 +1,12 @@
+import Fish from "./Fish";
 import styles from "./Pond.module.css";
 
 export default function Pond() {
-  return <div class={styles.pond}></div>;
+  return (
+    <>
+      <div class={styles.pond}>
+        <Fish />
+      </div>
+    </>
+  );
 }
