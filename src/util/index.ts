d93f86371c5b1e6fa15ebdca763fd6bf3b378203
JasperCDev <jasperischauvin@gmail.com>
2022-06-29 18:34:38 -0500

    tweaks

--- src/util/index.ts
+++ src/util/index.ts
@@ -7,15 +7,27 @@ export function swimToFood(
   const { x: fishX, y: fishY } = fishPos;
   const { x: foodX, y: foodY } = fishFood;
 
-  const differenceX = foodX - fishX;
-  const differenceY = foodY - fishY;
+  let differenceX = foodX - fishX;
+  let differenceY = foodY - fishY;
+
+  if (differenceX > 0) {
+    differenceX -= 30;
+  } else {
+    differenceX += 30;
+  }
+
+  if (differenceY > 0) {
+    differenceY -= 30;
+  } else {
+    differenceY += 30;
+  }
 
   const distance = Math.sqrt(
     Math.abs(foodX - fish.getBoundingClientRect().x) ** 2 +
       Math.abs(foodY - fish.getBoundingClientRect().y) ** 2
   );
 
-  fish.style.transition = `transform ${distance / 100}s linear`;
+  fish.style.transition = `transform ${distance / 500}s linear`;
   fish.style.transform = `translate(${differenceX}px, ${differenceY}px)`;
   fish.ontransitionend = onEnd || null;
 }
