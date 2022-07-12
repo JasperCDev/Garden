f556d7fa617eec63db6c64c4ed9c3b8d921ccffd
JasperCDev <jasperischauvin@gmail.com>
2022-07-12 11:24:38 -0500

    remove pond temp

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,5 @@ import styles from "./App.module.css";
 import Pond from "./components/Pond";
 
 export default function App() {
-  return (
-    <div class={styles.App}>
-      <Pond />
-    </div>
-  );
+  return <div class={styles.App}>{/* <Pond /> */}</div>;
 }
