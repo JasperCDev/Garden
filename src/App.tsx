5ebf4232a65a732f45135d3fbaee88ef506087b6
JasperCDev <jasperischauvin@gmail.com>
2022-07-12 20:59:05 -0500

    integrate plant component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import styles from "./App.module.css";
 import Pond from "./components/Pond";
+import Plant from "./components/Plant";
 
 export default function App() {
-  return <div class={styles.App}>{/* <Pond /> */}</div>;
+  return (
+    <div class={styles.App}>
+      {/* <Pond /> */}
+      <Plant count={10000} />
+    </div>
+  );
 }
