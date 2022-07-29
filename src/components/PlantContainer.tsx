c3f6e133a2ae57676e668b227ec5543a92fc48fd
JasperCDev <jasperischauvin@gmail.com>
2022-07-29 17:58:17 -0500

    highlight hovered tile

--- src/components/PlantContainer.tsx
+++ src/components/PlantContainer.tsx
@@ -2,12 +2,15 @@ import Plant from "./Plant";
 import { editPlant, PlantObject } from "../stores/plants.store";
 import { animateCount } from "../util/animateCount";
 import styles from "./PlantContainer.module.scss";
+import { createSignal } from "solid-js";
 
 interface Props {
   plant: PlantObject;
 }
 
 export default function PlantContainer(props: Props) {
+  const [isHovered, setIsHovered] = createSignal(false);
+
   function waterPlant() {
     if (props.plant.water !== 0) return;
     editPlant({ ...props.plant, water: 300 }); // add water
@@ -15,9 +18,17 @@ export default function PlantContainer(props: Props) {
   }
 
   return (
-    <div class={styles.soil}>
+    <div
+      class={styles.soil}
+      style={{ "--border": isHovered() ? "1px solid black" : "none" }}
+    >
       {/* hitbox */}
-      <div class={styles.hitbox} onClick={waterPlant}></div>
+      <div
+        class={styles.hitbox}
+        onClick={waterPlant}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      ></div>
       <Plant plant={props.plant} />
     </div>
   );
