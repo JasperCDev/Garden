57af4f59daff5e77941aa5ccb24b4ca2a401a906
JasperCDev <jasperischauvin@gmail.com>
2022-10-01 15:13:10 -0500

    remove fade out soil function

--- GardenGameMain/src/animations/soil.ts
+++ GardenGameMain/src/animations/soil.ts
@@ -18,21 +18,3 @@ export function createSoilAnimation(plant: PlantObject) {
     },
   });
 }
-
-export function createFadeOutSoil(plant: PlantObject) {
-  addAnimation({
-    id: gameStore.animations.latestId + 1,
-    name: "fade out soil",
-    progress: 0,
-    start: 0,
-    end: 1,
-    range: 1,
-    value: 0,
-    startTime: null,
-    duration: 10000,
-    previousTimeStamp: null,
-    payload: {
-      plantId: plant.id,
-    },
-  });
-}
